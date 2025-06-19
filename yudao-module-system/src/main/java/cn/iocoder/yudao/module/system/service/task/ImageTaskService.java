a9162cb8b9b25c1670ae49c8f91e3668aa20f015
fangliu <liufang@vogvcells.com>
2025-06-19 23:56:26 +0800

    dev

--- yudao-module-system/src/main/java/cn/iocoder/yudao/module/system/service/task/ImageTaskService.java
+++ yudao-module-system/src/main/java/cn/iocoder/yudao/module/system/service/task/ImageTaskService.java
@@ -5,7 +5,9 @@ import cn.iocoder.yudao.module.system.controller.admin.task.vo.ImageTaskQueryReq
 import cn.iocoder.yudao.module.system.dal.dataobject.task.ImageTaskDO;
 import cn.iocoder.yudao.module.system.dal.mysql.task.ArticleMapper;
 import cn.iocoder.yudao.module.system.dal.mysql.task.ImageTaskMapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import javax.annotation.Resource;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,6 +22,15 @@ public class ImageTaskService {
 
   public PageResult<ImageTaskDO> pageQuery(ImageTaskQueryReqVO reqVO){
     return imageTaskMapper.selectPage(reqVO);
+  }
+
+  public Integer update(ImageTaskDO imageTaskDO) {
+    UpdateWrapper<ImageTaskDO> updateWrapper = new UpdateWrapper<>();
+    if (StringUtils.isNotBlank(imageTaskDO.getFirstImage())){
+      updateWrapper.set("first_image", imageTaskDO.getFirstImage());
+    }
+    updateWrapper.eq("id", imageTaskDO.getId());
+    return imageTaskMapper.update(updateWrapper);
   }
 
 }
