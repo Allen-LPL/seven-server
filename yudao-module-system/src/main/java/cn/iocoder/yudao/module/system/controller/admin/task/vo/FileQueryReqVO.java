6c8aa3cf0af575d3861da0d5e6b7a8eac184fe6f
fangliu <liufang@vogvcells.com>
2025-06-22 14:30:35 +0800

    add

---
+++ yudao-module-system/src/main/java/cn/iocoder/yudao/module/system/controller/admin/task/vo/FileQueryReqVO.java
@@ -0,0 +1,28 @@
+package cn.iocoder.yudao.module.system.controller.admin.task.vo;
+
+
+import cn.iocoder.yudao.framework.common.pojo.PageParam;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Schema(description = "管理后台 - 邮箱账号分页 Request VO")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class FileQueryReqVO extends PageParam {
+
+  @Schema(description = "创建人", example = "1")
+  private Long creatorId;
+
+  @Schema(description = "上传开始时间")
+  private Long startTime;
+
+  @Schema(description = "上传结束时间")
+  private Long endTime;
+
+  @Schema(description = "杂志社")
+  private String articleJournal;
+
+}
