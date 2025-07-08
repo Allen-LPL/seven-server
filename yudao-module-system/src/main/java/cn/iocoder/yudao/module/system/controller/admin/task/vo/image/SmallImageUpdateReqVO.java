150ef211c7c0435fef5d55cf200b5a17e22b67a6
fangliu <liufang@vogvcells.com>
2025-07-08 15:00:10 +0800

    dev

---
+++ yudao-module-system/src/main/java/cn/iocoder/yudao/module/system/controller/admin/task/vo/image/SmallImageUpdateReqVO.java
@@ -0,0 +1,22 @@
+package cn.iocoder.yudao.module.system.controller.admin.task.vo.image;
+
+
+import cn.iocoder.yudao.framework.common.pojo.PageParam;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Schema(description = "管理后台 - 邮箱账号分页 Request VO")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class SmallImageQueryReqVO extends PageParam {
+
+  @Schema(description = "上传开始时间")
+  private Long startTime;
+
+  @Schema(description = "上传结束时间")
+  private Long endTime;
+
+}
